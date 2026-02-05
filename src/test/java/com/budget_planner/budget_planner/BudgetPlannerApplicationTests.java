c73a839570bb09f4b497132f27934ed8cba4b46d
nathan-vgh <nathgh04@gmail.com>
2026-02-04 18:31:18 -0600

    Feat initial commit

---
+++ src/test/java/com/budget_planner/budget_planner/BudgetPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budget_planner.budget_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
